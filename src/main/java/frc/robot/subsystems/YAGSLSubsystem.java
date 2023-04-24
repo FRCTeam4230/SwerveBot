e36343b7faa51d3294d2c8370274e828168e2df9
Jian <jiansundvall@gmail.com>
2023-04-24 17:25:44 -0500

    Testing out yagsl

---
+++ src/main/java/frc/robot/subsystems/YAGSLSubsystem.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.io.File;
+import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.parser.SwerveParser;
+import swervelib.SwerveDrive;
+
+public class YAGSLSubsystem extends SubsystemBase {
+
+  File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
+  SwerveDrive swerveDrive  = new SwerveParser(swerveJsonDirectory).createSwerveDrive();
+
+  public YAGSLSubsystem() {}
+
+  @Override
+  public void periodic() {
+  }
+}
